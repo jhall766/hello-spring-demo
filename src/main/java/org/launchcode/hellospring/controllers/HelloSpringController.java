bf5a8a80777a1d02604c3e0afa73ea6981b2e2de
jhall766 <Jhall766@gmail.com>
2021-02-21 14:40:03 -0600

    Added Thymeleaf Forms

--- src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
@@ -15,18 +15,19 @@ public class HelloSpringController {
 
     // Responds to /hello?name=LaunchCode
     @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
-    public String hello(@RequestParam String name, Model model) {
+    public String hello(@RequestParam String name,Model model) {
         String greeting = "Hello, " + name + "!";
-        model.addAttribute("greeting", greeting);
-        return "hello";
+        model.addAttribute("greeting",greeting);
+        return  "hello";
     }
 
     // Responds to /hello/LaunchCode
     @GetMapping("hello/{name}")
-    public String helloAgain(@PathVariable String name, Model model) {
+
+    public String helloAgain(@PathVariable String name,Model model) {
         String greeting = "Hello, " + name + "!";
-        model.addAttribute("greeting", greeting);
-        return "hello";
+        model.addAttribute("greeting",greeting);
+        return "Hello";
     }
 
     @GetMapping("form")
@@ -36,12 +37,16 @@ public class HelloSpringController {
 
     @GetMapping("hello-names")
     public String helloNames(Model model) {
+
         List<String> names = new ArrayList<>();
         names.add("LaunchCode");
         names.add("Java");
         names.add("JavaScript");
-        model.addAttribute("names", names);
+        model.addAttribute("names",names);
+
         return "hello-list";
     }
+
+
 
 }
